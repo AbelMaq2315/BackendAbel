5981d0e3c6d8ec50aa3f55a6d6f9036384d8e71c
EsainDG <48784351+EsainDG@users.noreply.github.com>
2022-09-30 19:42:34 -0500

    cambio

---
+++ Entiti/NuevaClase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo2.Entiti
+{
+    public class NuevaClase
+    {
+        //comentario
+        public int MyProperty { get; set; }
+
+    }
+}
